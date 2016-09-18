1f2e43fc6e37f51ced82d5c3b4b5f4cb7522bf8a
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2016-09-18 22:05:08 +0200

    add numba benchmarks

--- fib.py
+++ fib.py
@@ -28,23 +28,67 @@ def fibpy(num):
         return 1
     return fibpy(num - 2) + fibpy(num - 1)
 
-TESTS = (('rust_debug', fibrust_debug), ('rust_release', fibrust_release),
-         ('c_debug', fibc_debug), ('c_release', fibc_release),
-         ('py', fibpy))
+# ---- PYTHON NUMBA -----------------------------
+
+import numba
+
+@numba.jit
+def fibnumba_lazy(num):
+    if num < 2:
+        return 1
+    return fibnumba_lazy(num - 2) + fibnumba_lazy(num - 1)
+
+#fibnumba_eager = numba.jit(fibpy, numba.int64(numba.int64))
+@numba.jit(numba.int64(numba.int64))
+def fibnumba_eager(num):
+    if num < 2:
+        return 1
+    return fibnumba_eager(num - 2) + fibnumba_eager(num - 1)
+
+@numba.jit(nogil=True)
+def fibnumba_lazy_nogil(num):
+    if num < 2:
+        return 1
+    return fibnumba_lazy_nogil(num - 2) + fibnumba_lazy_nogil(num - 1)
+
+#fibnumba_eager = numba.jit(fibpy, numba.int64(numba.int64))
+@numba.jit(numba.int64(numba.int64), nogil=True)
+def fibnumba_eager_nogil(num):
+    if num < 2:
+        return 1
+    return fibnumba_eager_nogil(num - 2) + fibnumba_eager_nogil(num - 1)
 
 
-def timeit(n):
-    import time
+TESTS = (
+    ('rust_debug', fibrust_debug),
+    ('rust_release', fibrust_release),
+    ('c_debug', fibc_debug),
+    ('c_release', fibc_release),
+    ('numba lazy', fibnumba_lazy),
+    ('numba_eager', fibnumba_eager),
+    ('numba lazy_nogil', fibnumba_lazy_nogil),
+    ('numba_eager_nogil', fibnumba_eager_nogil),
+    ('py', fibpy)
+)
+
+def timeall(n, verbose=True):
     for (name, f) in TESTS:
-        print("Starting {0}...".format(name))
-        s = time.time()
-        r = f(n)
-        dt = time.time() - s
-        print("{0} took {1:g}s".format(name, dt))
+#        if verbose: print("Starting {}...".format(name))
+        dt = timeit(f, n)
+        if verbose:print("{0} took {1:g}s".format(name, dt))
+
+def timeit(f, n):
+    s = time.time()
+    r = f(n)
+    return time.time() - s
 
 if __name__ == "__main__":
     from sys import argv
-    n = 35
+    n = 30
     if len(argv) > 1:
         n = int(argv[1])
-    timeit(n)
+
+    # warm up
+#    timeall(3, verbose=False)
+
+    timeall(n)
